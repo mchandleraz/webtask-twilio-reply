918e6bbefc7c7b40e86daae06c874274ed59314d
Matt Chandler <mcaz689@gmail.com>
2017-05-17 17:42:00 -0700

    clean up var names for clarity.

--- replyHandler.js
+++ replyHandler.js
@@ -2,24 +2,24 @@ var twilio = require('twilio');
 var _ = require('lodash');
 
 module.exports = function(context, request, response) {
-  var messageBody = _.get(context, 'body.Body', '');
+  var incomingMessage = _.get(context, 'body.Body', '');
   var SID = _.get(context, 'secrets.SID');
   var TOKEN = _.get(context, 'secrets.TOKEN');
   var FROM = _.get(context, 'secrets.FROM');
 
   var client = new twilio(SID, TOKEN);
-  var body;
+  var responseBody;
 
-  switch (messageBody.toLowerCase()) {
+  switch (incomingMessage.toLowerCase()) {
     case 'done':
-      body = 'got done keyword';
+      responseBody = 'got done keyword';
       break;
     default:
-      body = 'received no keywords';
+      responseBody = 'received no keywords';
   }
 
   client.messages.create({
-    body: body,
+    body: responseBody,
     to: context.body.From,
     from: FROM
   })
